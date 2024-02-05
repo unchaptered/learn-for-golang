277d9c2bbbfc8424463a3b65c773b87bf654005b
unchaptered <workstation19961002@gmail.com>
2024-02-05 22:41:32 +0900

    Create : Golang Interactive Function

--- src/train/variable_casting/main.go
+++ src/train/variable_casting/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	interactive()
+	addNumber1 := interactive(os.Args[1], os.Args[2])
+	addNumber2, mulNumber2 := interactive_function(os.Args[1], os.Args[2])
+
+	fmt.Println("[main]", addNumber1)
+	fmt.Println("[main]", addNumber2, mulNumber2)
+
+	username := "Kevin"
+	fmt.Println("[main][pointer]", username)
+
+	pointer_function(username)
+
+	fmt.Println("[main][pointer]", username)
 }
