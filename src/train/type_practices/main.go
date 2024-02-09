244bf63c7e388eb400b80c9a200ac92b2cb00e84
unchaptered <workstation19961002@gmail.com>
2024-02-09 20:06:41 +0900

    Create : Golang romanToArabic Practice

--- src/train/type_practices/main.go
+++ src/train/type_practices/main.go
@@ -8,4 +8,8 @@ func main() {
 	fmt.Println("What's the Fibonacci sequence you want?")
 	fmt.Scanln(&num)
 	fmt.Println("The Fibonacci sequence is : ", fibonacci(num))
+
+	fmt.Println("MCLX is", romanToArabic("MCLX"))
+	fmt.Println("MCMXCIX is ", romanToArabic("MCMXCIX"))
+	fmt.Println("MCMZ is", romanToArabic("MCMZ"))
 }
