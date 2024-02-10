6d931fe41f5aa55686268e2e42d6940ce2758aea
unchaptered <workstation19961002@gmail.com>
2024-02-10 20:12:42 +0900

    Create : Golang LoggerFramework List

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Golang
 
 Golang 공부
+
+- ShellScript
+
+```shell
+go run -v $(go list ./...)
+```
+
+- Cmd
+
+```cmd
+for /f %i in ('go list ./...') do go run -v %i
+```
---
+++ src/train/logger_framework/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("https://github.com/sirupsen/logrus")
+	fmt.Println("https://github.com/rs/zerolog")
+	fmt.Println("https://github.com/uber-go/zap")
+	fmt.Println("https://github.com/apex/log")
+}
